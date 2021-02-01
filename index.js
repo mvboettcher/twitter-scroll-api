a0bd68e720e57582ed9b9e7d3883f690d519c477
Max Boettcher <mvboettcher@gmail.com>
2021-02-01 11:19:48 -0500

    adds env vars, adds cors, updates req params

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-const express = require('express');
-const Twitter = require('twitter');
+const express = require('express')
+const cors = require('cors')
+const Twitter = require('twitter')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './.env' })
 
 const client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+})
+
 const defaults = {
-  screen_name: 'realDonaldTrump',
   tweet_mode: 'extended',
+  exclude_replies: false,
+  include_rts: true,
   count: 20,
-};
+}
 
-const app = express();
+const app = express()
 
-app.route('/:handle')
-  .get(function(req, res) {
-    const params = {
-      ...defaults,
-      max_id: req.query.max_id,
-      screen_name: req.params.handle,
-    };
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+app.use(cors())
+
+app.route('/:handle').get(function (req, res) {
+  const params = {
+    ...defaults,
+    max_id: req.query.max_id,
+    screen_name: req.params.handle,
+  }
+  client.get(
+    'statuses/user_timeline',
+    params,
+    function (error, tweets, response) {
       if (!error) {
-        res.json(tweets);
+        res.json(tweets)
       } else {
         console.error(error)
       }
-    });
-  });
+    }
+  )
+})
 
-app.listen(3000, function(error) {
-  console.log('Trump listening on port 3000');
-});
-
+app.listen(5000, function (error) {
+  console.log(' server running on port 5000')
+})
